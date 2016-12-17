787ff9d14eed063dc5f2fdc24b7c549e6be144ce
KonekoFox <isaeva-spb@inbox.ru>
2016-12-17 21:46:59 +0300

    layout 2

--- js/app.js
+++ js/app.js
@@ -6,17 +6,22 @@ var userNav = document.querySelector('.user-nav');
 
 mainMenuBtn.addEventListener('click', function(evt) {
   if (mainNav.classList.contains('main-nav--closed')
-  && userNav.classList.contains('user-nav--closed')) {
+  && userNav.classList.contains('user-nav--closed')
+  && mainMenuBtn.classList.contains('page-header__toggle--open')) {
     mainNav.classList.remove('main-nav--closed');
     userNav.classList.remove('user-nav--closed');
+    mainMenuBtn.classList.remove('page-header__toggle--open');
 
     mainNav.classList.add('main-nav--opened');
     userNav.classList.add('user-nav--opened');
+    mainMenuBtn.classList.add('page-header__toggle--close');
   } else {
     mainNav.classList.remove('main-nav--opened');
     userNav.classList.remove('user-nav--opened');
+    mainMenuBtn.classList.remove('page-header__toggle--close');
 
     mainNav.classList.add('main-nav--closed');
     userNav.classList.add('user-nav--closed');
+    mainMenuBtn.classList.add('page-header__toggle--open');
   }
 });
